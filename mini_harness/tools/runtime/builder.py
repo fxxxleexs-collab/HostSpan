868263c57e55e13843757c726194d8b623cd55a9
fxxxleexs <mega2mew@foxmail.com>
2026-08-15 17:49:49 +0800

    迁移terminal模块

--- mini_harness/tools/runtime/builder.py
+++ mini_harness/tools/runtime/builder.py
@@ -6,22 +6,10 @@ from mini_harness.tools.runtime.file import build_file_tools
 from mini_harness.tools.runtime.remote import build_remote_tools
 from mini_harness.tools.runtime.sync import build_sync_tools
 from mini_harness.tools.runtime.task import build_task_tools
+from mini_harness.tools.runtime.terminal import build_terminal_tools
 
 
 def build_runtime_tools(runtime: HarnessRuntimeClient) -> list[AgentTool]:
-    from mini_harness.tools.adapter import (
-        ActivateTerminalSessionTool,
-        CloseTerminalTool,
-        InspectTerminalSessionTool,
-        ListTerminalSessionsTool,
-        ObserveTerminalTool,
-        OpenTerminalTool,
-        RequestHumanTerminalInputTool,
-        RunInSessionTool,
-        SendTerminalControlTool,
-        SendTerminalInputTool,
-    )
-
     remote_tools = build_remote_tools(runtime)
     ensure_remote_tool, request_ssh_connection_tool = remote_tools
     return [
@@ -30,19 +18,7 @@ def build_runtime_tools(runtime: HarnessRuntimeClient) -> list[AgentTool]:
         ensure_remote_tool,
         *build_sync_tools(runtime),
         request_ssh_connection_tool,
-        OpenTerminalTool(runtime),
-        OpenTerminalTool(runtime, "open_local_terminal", fixed_target="local"),
-        OpenTerminalTool(runtime, "open_remote_terminal", fixed_target="remote"),
-        ListTerminalSessionsTool(runtime),
-        InspectTerminalSessionTool(runtime),
-        ActivateTerminalSessionTool(runtime),
-        ObserveTerminalTool(runtime),
-        SendTerminalInputTool(runtime),
-        RequestHumanTerminalInputTool(runtime),
-        SendTerminalControlTool(runtime),
-        RunInSessionTool(runtime, "run_terminal_command"),
-        RunInSessionTool(runtime),
-        CloseTerminalTool(runtime),
+        *build_terminal_tools(runtime),
     ]
 
 
